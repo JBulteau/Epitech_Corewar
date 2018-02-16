f871b31b5f1dae3c3340e96130066cdec39a6db8
NahisWayard <lilian.arago@outlook.fr>
2018-02-16 02:43:22 +0100

    Added rev_endianness

---
+++ asm/src/endianness.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2017
+** endianness.c
+** File description:
+** Endianness functions
+*/
+
+#include "my.h"
+
+/* Returns 0 if system is in Little Endian and 1 otherwise */
+int check_endianness(void) {
+	if (1 >> sizeof(int))
+		return (0);
+	else
+		return (1);
+}
+
+/* Changes the endiannes of an int */
+int rev_endiannes(int nb) {
+	int res = 0;
+
+	for (int i = 0; i < 4; i++) {
+		if (res)
+			res = res << 8;
+		res += nb >> (8 * i) & (0xff);
+	}
+	return (res);	
+}
