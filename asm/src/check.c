723ae738ddff393c1f93d402e56add76230022ee
bernardHzCEZI <romain.bernard@epitech.eu>
2018-02-20 00:38:13 +0100

    add find instruction

--- asm/src/check.c
+++ asm/src/check.c
@@ -9,3 +9,17 @@ int check_args(int op_code, char *args)
 {
 	return (0);
 }
+
+int find_instru(char *str)
+{
+	int i = -1;
+
+	while(str[++i] != '\0' && str[i] != ' ');
+	str[i] = '\0';
+	i = -1;
+	while(op_tab[++i] != NULL && strcmp(op_tab[i].mnemonique, str) != 0);
+	if (op_tab[i] == NULL)
+		return (-1);
+	else
+		return (i - 1);
+}
