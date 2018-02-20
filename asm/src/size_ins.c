dda6860498233cb93233dab0d2127d2c68f1b54f
NahisWayard <lilian.arago@outlook.fr>
2018-02-20 04:38:44 +0100

    Fix for size on empty ins

--- asm/src/size_ins.c
+++ asm/src/size_ins.c
@@ -14,6 +14,8 @@ int size(in_struct_t op)
 {
 	int size = 1;
 
+	if (op.op_code == 0)
+		return (0);
 	if (op.args_types != 0)
 		size++;
 	if ((op.op_code == 10) || (op.op_code == 11) || (op.op_code == 14))
