5ed91fe474b7e64ec4da7ac7fbe959dd5f79ddae
Guillaume Jeanne <guillaume.jeanne@epitech.eu>
2018-02-20 04:17:01 +0100

    application of main loop

--- asm/src/fill_linked_list.c
+++ asm/src/fill_linked_list.c
@@ -19,28 +19,30 @@ int parsing(node_t *first, char **buffer, int fd)
 	int check = 0;
 	node_t *new = NULL;
 
-	for (; *buffer != NULL && (*buffer[0] == '\0' || *buffer[0] == '#'); \
-(*buffer = get_next_line(fd))); // main for ??
-	new = malloc(sizeof(*new));
-	for (; *buffer[inc] != '\0'; inc++) {
-		if (*buffer[inc] == '#' || *buffer[inc] == '%' || \
+	for (; *buffer != NULL ; *buffer = get_next_line(fd)) {
+		if (*buffer[0] == '\0' || *buffer[0] == '#')
+			continue;
+		new = malloc(sizeof(*new));
+		for (; *buffer[inc] != '\0'; inc++) {
+			if (*buffer[inc] == '#' || *buffer[inc] == '%' || \
 *buffer[inc] == ',')
-			return(-6);
-		if (*buffer[inc] == ' ' || *buffer[inc] == ':')
-			break;
-		if ((check = check_label_chars(buffer, inc)) < 0)
-			return (check);
-	}
-	if (*buffer[inc] != ':')
-		check = find_instru(*buffer);
-	if (check == -1 || *buffer[inc] == '\0' || (*buffer[inc] == ':' \
+				return(-6);
+			if (*buffer[inc] == ' ' || *buffer[inc] == ':')
+				break;
+			if ((check = check_label_chars(buffer, inc)) < 0)
+				return (check);
+		}
+		if (*buffer[inc] != ':')
+			check = find_instru(*buffer);
+		if (check == -1 || *buffer[inc] == '\0' || (*buffer[inc] == ':' \
 && *buffer[inc + 1] != ' '))
-		return (-6);
-	first->next = new;
-	new->info.op_code = check;
-	if ((check = check_args(check, *buffer + inc + 1, &(new->info))) < 0)
-		return(check);
-	first = new;
+			return (-6);
+		first->next = new;
+		new->info.op_code = check;
+		if ((check = check_args(check, *buffer + inc + 1, &(new->info))) < 0)
+			return(check);
+		first = new;
+	}
 	return (0);
 }
 
