0f844d7954dd0d58d6ea3991aaa0c373bf688228
Guillaume Jeanne <guillaume.jeanne@epitech.eu>
2018-02-20 03:15:28 +0100

    rect malloc in the loop for the new case of ll (linked list for next push)

--- asm/src/fill_linked_list.c
+++ asm/src/fill_linked_list.c
@@ -17,10 +17,11 @@ int parsing(node_t *first, char **buffer, int fd)
 {
 	int inc = 0;
 	int check = 0;
-	node_t *new = malloc(sizeof(*new));
+	node_t *new = NULL;
 
 	for (; *buffer != NULL && (*buffer[0] == '\0' || *buffer[0] == '#'); \
 (*buffer = get_next_line(fd))); // main for ??
+	new = malloc(sizeof(*new));
 	for (; *buffer[inc] != '\0'; inc++) {
 		if (*buffer[inc] == '#' || *buffer[inc] == '%' || \
 *buffer[inc] == ',')
@@ -35,8 +36,10 @@ int parsing(node_t *first, char **buffer, int fd)
 	if (check == -1 || *buffer[inc] == '\0' || (*buffer[inc] == ':' \
 && *buffer[inc + 1] != ' '))
 		return (-6);
-	//fill hard code here;
+	first->next = new;
+	new->info.op_code = check;
 	if ((check = check_args(check, *buffer + inc + 1, &(new->info))) < 0)
 		return(check);
+	first = new;
 	return (0);
 }
