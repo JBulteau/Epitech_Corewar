fe76e25a87f286d3230301e801171b92ab2429ac
Lilian ARAGO <lilian.arago@epitech.eu>
2018-02-28 21:16:28 +0100

    Load indexes

--- corewar/src/read_instru.c
+++ corewar/src/read_instru.c
@@ -14,7 +14,7 @@ int load_n_arena(unsigned char *arena, int pos, int size)
 
 	for (int i = 0; i < size; i++) {
 		res = res << 8;
-		printf("-> %x\n", res);	
+		printf("-> %x\n", res);
 		res += arena[(pos + i) % MEM_SIZE];
 	}
 	printf("%x\n", res);
@@ -36,7 +36,7 @@ void read_one_arg(unsigned char *arena, int pos, in_struct_t *op)
 	op->args[0] = load_n_arena(arena, pos + 1, 2);
 }
 
-void load_args(char *arena, in_struct_t *op)
+void load_args(unsigned char *arena, in_struct_t *op)
 {
 	int arg = 0;
 	int cursor = 2;
@@ -58,6 +58,24 @@ void load_args(char *arena, in_struct_t *op)
 	}
 }
 
+void load_index(unsigned char *arena, in_struct_t *op)
+{
+	int arg = 0;
+	int cursor = 2;
+
+	for (int i = 0; i < 4; i++) {
+		arg = op->arg_type >> 6 - (i * 2) & 0b11;
+		if (arg == 0b01) {
+			op->args[i] = load_n_arena(arena, cursor, 1);
+			cursor += 1;
+		}
+		if (arg == 0b11 || arg == 0b10) {
+			op->args[i] = load_n_arena(arena, cursor, 2);
+			cursor += 2;
+		}
+	}
+}
+
 in_struct_t read_instru(unsigned char *arena, int pos)
 {
 	in_struct_t op;
@@ -69,11 +87,15 @@ in_struct_t read_instru(unsigned char *arena, int pos)
 	if ((op.op_code < 1) || (op.op_code > 16)) {
 		op.op_code = -1;
 		return (op);
-	} else if ((op.op_code == 1) || (op.op_code == 9) || (op.op_code == 12) || (op.op_code == 15) || (op.op_code == 16)) {
+	}
+	if ((op.op_code == 1) || (op.op_code == 9) || (op.op_code == 12) || (op.op_code == 15) || (op.op_code == 16)) {
 		read_one_arg(arena, pos, &op);
 		return (op);
 	}
 	op.arg_type = load_n_arena(arena, pos + 1, 1);
-	load_args(arena, &op);
+	if ((op.op_code == 0x0e) || (op.op_code == 0x0b) || (op.op_code == 0x0a))
+		load_index(arena, &op);
+	else
+		load_args(arena, &op);
 	return (op);
 }
