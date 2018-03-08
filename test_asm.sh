b83a099b7864bf0a7411c8529f366ab61228d5d9
Lilian ARAGO <lilian.arago@epitech.eu>
2018-03-08 13:14:48 +0100

    Run test on one single file.s

--- test_asm.sh
+++ test_asm.sh
@@ -1,13 +1,21 @@
  #!/bin/bash
 
-CHAMPIONS_PATH=./champions/*.s
+if [ $# -eq 0 ]
+then
+	CHAMPIONS_PATH=./champions/*.s
+else
+	#for var in "$@"; do
+	#	echo "$var"
+	#done
+	CHAMPIONS_PATH=$1
+fi
 
 rm -rf res
 mkdir res
 rm -f res/*.res
 
-dif = 0
-no_dif = 0
+dif=0
+no_dif=0
 for f in $CHAMPIONS_PATH; do
 	echo "---------------------------------------------------------"
 	echo "Compiling "$f
