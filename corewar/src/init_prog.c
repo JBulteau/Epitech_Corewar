78e7d345ae78499bdb45eb9827c8c53861e3aeb2
Lilian ARAGO <lilian.arago@epitech.eu>
2018-03-11 23:15:15 +0100

    Removed comment

--- corewar/src/init_prog.c
+++ corewar/src/init_prog.c
@@ -66,7 +66,6 @@ int get_program_size(prog_name_t *prog)
 		return (-1);
 	if (read(fd, &size, sizeof(int)) == -1)
 		return (-1);
-	//TODO CHECK PROG SIZE AFTER LOAD
 	prog->size = rev_endiannes(size);
 	free(str);
 	close(fd);
