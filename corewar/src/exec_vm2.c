7e33380636f996d863be030347e8181668f8b525
Guillaume Jeanne <guillaume.jeanne@epitech.eu>
2018-03-11 21:56:42 +0100

    some errors rect on exec functions

--- corewar/src/exec_vm2.c
+++ corewar/src/exec_vm2.c
@@ -15,22 +15,33 @@ int xor(unsigned char *arena, prog_t *prog)
 {
 	int type_arg_1 = prog->instr.arg_type >> 6 & 0b11;
 	int type_arg_2 = prog->instr.arg_type >> 4 & 0b11;
+	int reg0 = prog->instr.args[2];
+	int reg1 = prog->instr.args[1];
+	int reg2 = prog->instr.args[0];
 
-	if ((int*)arena && type_arg_1 == 1) {
-		if (type_arg_2 == 1)
+	if (reg0 < 1 || reg0 > REG_NUMBER)
+		return (-1);
+	(void)arena;
+	if (type_arg_1 == 1) {
+		if (reg2 < 1 || reg2 > REG_NUMBER)
+			return (-1);
+		if (type_arg_2 == 1) {
+			if (reg1 < 1 || reg1 > REG_NUMBER)
+				return (-1);
 			prog->reg[(prog->instr.args[2]) - 1] = \
 prog->reg[(prog->instr.args[0]) - 1] ^ prog->reg[(prog->instr.args[1]) - 1];
-		else
+		} else
 			prog->reg[(prog->instr.args[2]) - 1] = \
 prog->reg[(prog->instr.args[0]) - 1] ^ prog->instr.args[1];
-	} else {
-		if (type_arg_2 == 1)
+	} else
+		if (type_arg_2 == 1) {
+			if (reg1 < 1 || reg1 > REG_NUMBER)
+				return (-1);
 			prog->reg[(prog->instr.args[2]) - 1] = \
 prog->instr.args[0] ^ prog->reg[(prog->instr.args[1]) - 1];
-		else
-			prog->reg[(prog->instr.args[0]) - 1] = \
+		} else
+			prog->reg[(prog->instr.args[2]) - 1] = \
 prog->instr.args[0] ^ prog->instr.args[1];
-	}
 	prog->carry = 1;
 	return (0);
 }
