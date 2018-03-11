e32b989102a808c69040794366375dd0153245da
Lilian ARAGO <lilian.arago@epitech.eu>
2018-03-11 18:15:02 +0100

    Added test too many args

--- test_cor.sh
+++ test_cor.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+./asm/asm ./champions/live1.s
+
+check_return()
+{
+        if [ $? == $1 ]; then
+                echo "OK"
+        else
+                echo "NOPE"
+        fi
+}
+
 echo "---------------------------------------------------------"
 echo "Invalid file"
 echo "---------------------------------------------------------"
-./corewar/corewar a.cor b.cor c.cor d.cor
+./corewar/corewar notfound.cor b.cor c.cor d.cor
+check_return 84
+
+echo "---------------------------------------------------------"
+echo "Too many args"
+echo "---------------------------------------------------------"
+./corewar/corewar live1.cor live1.cor live1.cor live1.cor live1.cor
+check_return 84
+
+rm live1.cor
