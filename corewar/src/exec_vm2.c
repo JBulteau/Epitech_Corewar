89120ccb36d0141bc0c63a73004ee5212895890a
Guillaume Jeanne <guillaume.jeanne@epitech.eu>
2018-03-11 22:36:07 +0100

    more errors on function rect

--- corewar/src/exec_vm2.c
+++ corewar/src/exec_vm2.c
@@ -59,21 +59,31 @@ int zjmp(unsigned char *arena, prog_t *prog)
 int ldi(unsigned char *arena, prog_t *prog)
 {
 	int reg = prog->instr.args[2];
+	int reg1 = prog->instr.args[1];
+	int reg2 = prog->instr.args[0];
 	int type_arg_1 = prog->instr.arg_type >> 6 & 0b11;
 	int type_arg_2 = prog->instr.arg_type >> 4 & 0b11;
 
-	if (type_arg_1 == 1)
-		if (type_arg_2 == 1)
+	if (reg < 1 || reg > REG_NUMBER)
+		return (-1);
+	if (type_arg_1 == 1) {
+		if (reg2 < 1 || reg2 > REG_NUMBER)
+			return (-1);
+		if (type_arg_2 == 1) {
+			if (reg1 < 1 || reg1 > REG_NUMBER)
+				return (-1);
 			prog->reg[reg - 1] = arena[(prog->pc + prog->reg[prog->\
 instr.args[0] - 1] + prog->reg[prog->instr.args[1] - 1]) % IDX_MOD];
-		else
+		} else
 			prog->reg[reg - 1] = arena[(prog->pc + prog->reg[prog->\
 instr.args[0] - 1] + prog->instr.args[1]) % IDX_MOD];
-	else
-		if (type_arg_2 == 1)
+	} else
+		if (type_arg_2 == 1) {
+			if (reg1 < 1 || reg2 > REG_NUMBER)
+				return (-1);
 			prog->reg[reg - 1] = \
 arena[(prog->instr.args[0] + prog->reg[prog->instr.args[1]]) % IDX_MOD];
-		else
+		} else
 			prog->reg[reg - 1] = \
 arena[(prog->instr.args[0] + prog->instr.args[1]) % IDX_MOD];
 	prog->carry = 1;
@@ -83,24 +93,32 @@ arena[(prog->instr.args[0] + prog->instr.args[1]) % IDX_MOD];
 int sti(unsigned char *arena, prog_t *prog)
 {
 	int reg = prog->instr.args[0];
+	int reg1 = prog->instr.args[1];
+	int reg2 = prog->instr.args[2];
 	int type_arg_1 = prog->instr.arg_type >> 4 & 0b11;
 	int type_arg_2 = prog->instr.arg_type >> 2 & 0b11;
-	 
+
 	if (reg < 0 || reg > REG_NUMBER)
 		return (-1);
-	if (type_arg_1 == 1)
-		if (type_arg_2 == 1)
+	if (type_arg_1 == 1) {
+		if (reg1 < 1 || reg1 > REG_NUMBER)
+			return (-1);
+		if (type_arg_2 == 1) {
+			if (reg2 < 1 || reg2 > REG_NUMBER)
+				return (-1);
 			cp_reg_to_arena(arena, prog->pc + (prog->reg[prog->ins\
 tr.args[1] - 1] + prog->reg[prog->instr.args[2]] - 1) % IDX_MOD, prog->reg\
 [reg - 1]);
-		else
+		} else
 			cp_reg_to_arena(arena, prog->pc + (prog->reg[prog->ins\
 tr.args[1] - 1] + prog->instr.args[2]) % IDX_MOD, prog->reg[reg - 1]);
-	else
-		if (type_arg_2 == 1)
+	} else
+		if (type_arg_2 == 1) {
+			if (reg2 < 1 || reg2 > REG_NUMBER)
+				return (-1);
 			cp_reg_to_arena(arena, prog->pc + (prog->instr.args[1\
 ] + prog->reg[prog->instr.args[2] - 1]) % IDX_MOD, prog->reg[reg - 1]);
-		else
+		} else
 			cp_reg_to_arena(arena, prog->pc + (prog->instr.args[1\
 ] + prog->instr.args[2]) % IDX_MOD, prog->reg[reg - 1]);
 	return (0);
